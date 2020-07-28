ec94169edde318038fc06f1742451767e0141d79
Malte Hübner <maltehuebner@gmx.org>
2020-07-28 14:06:46 +0200

    Remove old WebServerBundle.

--- config/bundles.php
+++ config/bundles.php
@@ -8,6 +8,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
