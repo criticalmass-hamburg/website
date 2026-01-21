3a4b8ed629204ed63b4f2d24090e61b07a5ad09c
Malte Hübner <maltehuebner@gmx.org>
2026-01-21 11:53:52 +0100

    Switch from JMS to Symfony Serializer.

--- config/bundles.php
+++ config/bundles.php
@@ -6,6 +6,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
